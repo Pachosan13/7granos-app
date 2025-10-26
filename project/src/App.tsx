b8abb4bec97c2d09285421494885a6392bc38156
Pachosan13 <fransan13680@gmail.com>
2025-10-26 17:30:20 -0500

    Update App.tsx

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -11,7 +11,7 @@ import { Contabilidad } from './pages/Contabilidad';
 import { IniciarSesion } from './pages/Auth/IniciarSesion';
 
 // Payroll (UI read-only, detrás de feature flags)
-import { PeriodsPage as Periodos } from './pages/payroll/PeriodsPage';
+import { Periodos as Periodos } from './pages/payroll/Periodos';
 import { EmployeesPage } from './pages/payroll/EmployeesPage';
 import { AttendancePage } from './pages/payroll/AttendancePage';
 
